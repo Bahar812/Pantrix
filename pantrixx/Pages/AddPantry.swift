fafcb6ef2f848ec1bc0113eebfd26ccc92a2a108
Bahar812 <baharalhamid410@gmail.com>
2024-11-23 04:18:10 +0700

    Add Pantry Manually

---
+++ pantrixx/Pages/AddPantry.swift
@@ -0,0 +1,18 @@
+//
+//  AddPantry.swift
+//  pantrixx
+//
+//  Created by MacBook Pro on 23/11/24.
+//
+
+import SwiftUI
+
+struct AddPantry: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddPantry()
+}
